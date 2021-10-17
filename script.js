fb6969c40a68a29f87900fa014f2dbf8b8954a9a
mohamadabouchahine <91345393+mohamadabouchahine@users.noreply.github.com>
2021-10-17 17:30:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,170 @@
+class Player{
+    constructor(){
+        
+        this.audio_file = document.getElementById("audio_file");
+        this.isPlayed;
+
+        this.play = document.getElementById("play");
+        this.play.addEventListener("click",()=>{this.play_pause();});
+
+        this.audios =[];
+        this.audios[0] = "./audio/amjad.mp3";
+        this.audios[1] = "./audio/darb.mp3";
+        this.audios[2] = "./audio/kabiro.mp3";
+        this.audios[3] = "./audio/ps.mp3";
+    
+        this.names = [];
+        this.names[0] = "Track 1";
+        this.names[1] = "Track 2";
+        this.names[2] = "Track 3";
+        this.names[3] = "Track 4";
+        
+        
+        this.numberAudio =0;
+
+        this.next = document.getElementById("next");
+        this.next.addEventListener("click",()=>{
+            this.next_audio();
+        });
+           
+
+        this.back = document.getElementById("back");
+        this.back.addEventListener("click",()=>{
+            
+          this.back_audio();  
+         
+        });
+
+        this.setSource();
+    }
+    play_pause(){
+        if(this.isPlayed==false){
+            this.play.src = "./image/pause.png";
+            this.audio_file.play();
+            this.isPlayed=true;
+        }
+        else{
+            this.play.src = "./image/play.png";
+            this.audio_file.pause();
+            this.isPlayed=false;
+        }
+    }
+
+    next_audio(){
+         if(this.numberAudio<this.audios.length-1){
+                ++this.numberAudio;
+                this.isPlayed = false; 
+                }
+         else   {
+                this.numberAudio = 0;
+                }
+                this.setSource();
+    }
+    back_audio(){
+        if(this.numberAudio>0){
+            --this.numberAudio;
+            this.isPlayed = false; 
+         }
+         else{
+            this.numberAudio = this.audios.length -1;
+            
+         }
+      
+         this.setSource();
+    }
+    setSource(){
+ 
+    document.getElementById("radio-title").innerHTML = this.names[this.numberAudio];
+    document.getElementById("audio_file").src = this.audios[this.numberAudio];
+
+    this.play_pause();
+    }
+
+}
+onload = new Player();
+
+class Speed_Volume{
+    constructor(){
+        this.audio_file = document.getElementById("audio_file");
+
+        var speed = document.getElementById("speed");
+        speed.addEventListener("change",()=>{
+         this.audio_file.playbackRate = speed.value / 100;
+        });
+       
+    }
+
+}
+onload = new Speed_Volume();
+class Volume{
+    constructor(){
+
+        this.audio_file = document.getElementById("audio_file");
+
+        var volume = document.getElementById("volume");
+        this.audio_file.volume= 50/100;
+        
+        volume.addEventListener("change",()=>{
+         this.audio_file.volume = volume.value / 100 ;
+        } );
+    }
+
+}
+onload = new Volume();
+
+
+
+class Colors{
+    constructor(){
+        if(localStorage.getItem("SaveColor")==null){
+    
+            document.body.style.background = "linear-gradient(to right, #00d2ff, #928dab)";
+       
+        }
+
+    this.color1 = document.getElementById("color-1");
+    this.color1.addEventListener("click",()=>{
+        this.selectColor("color-1");
+    });
+
+    this.color2 = document.getElementById("color-2");
+    this.color2.addEventListener("click",()=>{
+        this.selectColor("color-2");
+    });
+
+    this.color3 = document.getElementById("color-3");
+    this.color3.addEventListener("click",()=>{
+        this.selectColor("color-3");
+    });
+
+    this.color4 = document.getElementById("color-4");
+    this.color4.addEventListener("click",()=>{
+        this.selectColor("color-4");
+    });
+
+    this.selectColor(localStorage.getItem("SaveColor"));
+    }
+
+    selectColor(color){
+       
+       
+        if(color == "color-1"){
+            
+            document.body.style.background = "rgb(9 41 16)";
+        }
+        else if(color == "color-2"){
+            document.body.style.background ="rgb(81 1 6)";
+        }
+
+        else if(color == "color-3"){
+            document.body.style.background ="#6b6b6b";
+        }
+
+        else if(color == "color-4"){
+            document.body.style.background ="rgb(7 7 7)";
+        }
+        localStorage.setItem("SaveColor",color);
+    }
+
+}
+onload = new Colors();
